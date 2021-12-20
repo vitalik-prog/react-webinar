bce0187ef87096faa9858e7ab3d5cbd0405e72bf
Vitaliy <270vitalik270@gmail.com>
2021-12-20 22:00:01 +0200

    deleted redundant code

--- src/components/navbar/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {NavLink} from "react-router-dom";
-
-const Navbar = () => {
-  return <NavLink to={`/`}>Главная</NavLink>;
-};
-
-export default React.memo(Navbar);
